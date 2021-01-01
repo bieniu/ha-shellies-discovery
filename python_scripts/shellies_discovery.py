df91e1eb45202e90b497d5cd20386ce95f08342e
Maciej Bieniek <maciej.bieniek@gmail.com>
2021-01-01 17:19:07 +0100

    Fix firmware update sensor

--- python_scripts/shellies_discovery.py
+++ python_scripts/shellies_discovery.py
@@ -955,7 +955,7 @@ if model_id == MODEL_SHELLYMOTION_ID or dev_id_prefix == MODEL_SHELLYMOTION_PREF
         TPL_VIBRATION,
         TPL_CHARGER,
     ]
-    bin_sensors_topics = [TOPIC_STATUS, TOPIC_STATUS, TOPIC_STATUS, TOPIC_STATUS]
+    bin_sensors_topics = [TOPIC_INFO, TOPIC_STATUS, TOPIC_STATUS, TOPIC_STATUS]
 
 if model_id == MODEL_SHELLYGAS_ID or dev_id_prefix == MODEL_SHELLYGAS_PREFIX:
     model = MODEL_SHELLYGAS
